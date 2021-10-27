6e6032451f2777a5946918899d4ab7545b5b05ed
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-27 18:50:48 +0200

    Blogapp: blog add form test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,3 +35,16 @@ Cypress.Commands.add('login', ({ username, password }) => {
     cy.visit('http://localhost:3000')
   })
 })
+
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/api/blogs',
+    body: { title, author, url },
+    headers: {
+      'Authorization': `bearer ${window.localStorage.getItem(
+        'userLoggedIntoBlogapp').token}`
+    }
+  })
+  cy.visit('http://localhost:3000')
+})
